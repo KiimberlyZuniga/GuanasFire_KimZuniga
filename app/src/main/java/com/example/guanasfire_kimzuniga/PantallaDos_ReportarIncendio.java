36e7fe6882d1daf0c7ff711587468cf52e728fb5
KiimberlyZuniga <kiimzuniiga@gmail.com>
2020-06-19 20:02:29 -0600

    Activitys mas Login

---
+++ app/src/main/java/com/example/guanasfire_kimzuniga/PantallaDos_ReportarIncendio.java
@@ -0,0 +1,14 @@
+package com.example.guanasfire_kimzuniga;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantallaDos_ReportarIncendio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_dos__reportar_incendio);
+    }
+}
